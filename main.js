100b89ae7e6593ee52172251d586548a3220b31d
JerryBULL <65128468+jerryBULL@users.noreply.github.com>
2020-06-29 15:59:16 +0800

    version_1.0
    1.AES加解密功能
    2.RWD

--- main.js
+++ main.js
@@ -12,6 +12,24 @@ function Decrypt(){
     Ciphertext = document.getElementById("Ciphertext").value;
     pwd = document.getElementById("Decrypt_pwd").value;
     let Decrypt_Plaintext = CryptoJS.AES.decrypt(Ciphertext,pwd).toString(CryptoJS.enc.Utf8);
-    document.getElementById("Decrypt_Plaintext").innerHTML = Decrypt_Plaintext;
+    if(Decrypt_Plaintext === "")
+    {
+        alert("密碼錯誤");
+    }
+    else
+    {
+        document.getElementById("Decrypt_Plaintext").innerHTML = Decrypt_Plaintext;
+    }
     document.getElementById("Decrypt_pwd").value = "";
 }
+
+$(document).ready(function () {
+    //複製功能
+    $( "#Decrypt_Plaintext,#Encrypt_Ciphertext" ).on( "click", function() {
+        let text = $( this ).select();
+        document.execCommand("copy");
+        $('#copy').fadeIn(400,function(){
+            $("#copy").fadeOut(400);
+        })
+    });
+});
